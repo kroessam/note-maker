8a9107b7204a585b9785a36310dc38d8aee9c27a
Samuel Kroes <kroessamuel@gmail.com>
2024-08-06 15:47:49 -0700

    Navbar Styled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,17 @@
 const Header = () => {
-  return <header className="text-center">Header</header>;
+  return (
+    <header className="text-center">
+      <nav>
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>About</li>
+          <li>Sign In</li>
+          <li>Dashboard</li>
+          <li>Profile</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
