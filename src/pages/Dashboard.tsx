d7bc0e68d4a3e2efbdcdf075b95fb7b0d56f57d8
Samuel Kroes <kroessamuel@gmail.com>
2024-08-12 12:40:54 -0700

    Page Restyled to Align Editor & Preview

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-  const [note, setNote] = useState("Write Here!");
+  const [note, setNote] = useState("(Example Text)");
   const [notesList, setNotesList] = useState<string[]>([]);
 
   const getDate = () => {
@@ -20,8 +20,13 @@ const Dashboard = () => {
   const [date, setDate] = useState(getDate());
 
   const handleSave = () => {
-    setDate(getDate); // Update date (and time!) on save
-    setNotesList([...notesList, `${date}: ${note}`]); // Add note w/ date to list
+    if (note === "") {
+      alert("Note must not be blank!");
+    } else {
+      setDate(getDate); // Update date (and time!) on save
+      setNotesList([...notesList, `${date}: ${note}`]); // Add note w/ date to list
+      setNote(""); // Reset text input
+    }
   };
 
   return (
@@ -31,18 +36,23 @@ const Dashboard = () => {
           Back to Home
         </Link>
         <h1>Dashboard</h1>
-        <textarea
-          className="note"
-          value={note}
-          onChange={(e) => setNote(e.target.value)}
-        ></textarea>
-        <button className="btn" onClick={handleSave}>
+        <div className="write-container">
+          <div className="note-box">
+            <textarea
+              className="note"
+              value={note}
+              onChange={(e) => setNote(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="preview-box">
+            <h2 className="text-center">Note Preview</h2>
+            <h3>{date}</h3>
+            <p>{note}</p>
+          </div>
+        </div>
+        <button id="btn-save" className="btn" onClick={handleSave}>
           Save
         </button>
-        <hr />
-        <h2 className="text-center">Note Preview</h2>
-        <h3>{date}</h3>
-        <p>{note}</p>
         <hr />
         <h2 className="text-center">Past Notes</h2>
         {notesList.map((thisNote) => (
