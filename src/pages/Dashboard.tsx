f09db431e6e8d09bcd497252b6fcc4c88a029a63
Samuel Kroes <kroessamuel@gmail.com>
2024-08-12 10:34:47 -0700

    Add Button Toggling

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Dashboard = () => {
+  const [isAdding, setIsAdding] = useState(false);
+
+  const handleAddClick = () => {
+    setIsAdding(!isAdding);
+  };
+
   return (
     <>
       <main>
+        <Link className="btn" to="/">
+          Back to Home
+        </Link>
         <h1>Dashboard</h1>
+        <button
+          className={"btn" + (isAdding ? " btn-cancel" : "")}
+          onClick={handleAddClick}
+        >
+          {isAdding ? "Cancel" : "Add New"}
+        </button>
       </main>
     </>
   );
