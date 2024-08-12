430e4699ffa7b424799f1e6583f0948025b2b9bc
Samuel Kroes <kroessamuel@gmail.com>
2024-08-12 11:55:37 -0700

    Live Date & Note Preview

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,22 @@ import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   const [isAdding, setIsAdding] = useState(false);
+  const [note, setNote] = useState("Write Here!");
+  const [notesList, setNotesList] = useState([]);
+
+  const getDate = () => {
+    let date = new Date();
+
+    let formattedDate = date.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+
+    return formattedDate;
+  };
+
+  const [date, setDate] = useState(getDate());
 
   const handleAddClick = () => {
     setIsAdding(!isAdding);
@@ -16,11 +32,19 @@ const Dashboard = () => {
         </Link>
         <h1>Dashboard</h1>
         <button
-          className={"btn" + (isAdding ? " btn-cancel" : "")}
+          className={"btn text-center" + (isAdding ? " btn-cancel" : "")}
           onClick={handleAddClick}
         >
           {isAdding ? "Cancel" : "Add New"}
         </button>
+        <textarea
+          className="note"
+          value={note}
+          onChange={(e) => setNote(e.target.value)}
+        ></textarea>
+        <h2 className="text-center">Note Preview</h2>
+        <h3>{date}</h3>
+        <p>{note}</p>
       </main>
     </>
   );
